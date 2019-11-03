8c1405ebad49abca330b1dba953cad354c0aa10b
Max Eddy <maxiscool@gmail.com>
2019-11-03 13:01:45 -0800

    Update references.

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-scp -r certs/ $1:/root/docker-wikijs/certs/
-scp .env $1:/root/docker-wikijs/.env
+scp -r certs/ $1:/root/docker-bookstack/certs/
+scp .env $1:/root/docker-bookstack/.env
